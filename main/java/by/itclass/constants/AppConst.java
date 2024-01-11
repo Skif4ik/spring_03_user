f3fc24e0a4f910e8f6f77211f770243688de7262
skif- <44skif99@gmail.com>
2024-01-11 21:31:06 +0300

    Initial commit

---
+++ main/java/by/itclass/constants/AppConst.java
@@ -0,0 +1,8 @@
+package by.itclass.constants;
+
+public class AppConst {
+    public static final String ALL_USER_URL = "/all";
+    public static final String USER_LIST_ATTR = "userList";
+    public static final String USER_PAGE = "users";
+
+}
